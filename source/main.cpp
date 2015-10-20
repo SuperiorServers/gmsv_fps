17bd20dccff00f16da219cf44de660e922c059c9
Austin Thomas <A.Thomas@TheD3vine.net>
2015-10-20 18:01:14 -0500

    Removed unneeded code

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,19 @@
-#include "GarrysMod/Lua/Interface.h"
-#include <stdio.h>
+// gmsv_fps
+// made by thelastpenguin
 
-#include "edict.h"
-#include "eiface.h" // engine interface.h IVEngineServer needs this
+#include "GarrysMod/Lua/Interface.h"
+
 #include "engine/iserverplugin.h"
 #include "game/server/iplayerinfo.h"
 
-#include "Color.h"
-#include "tier0/dbg.h"
-
 #ifdef _LINUX
 	#define SERVER_LIB "./garrysmod/bin/server_srv.so"
-	#define ENGINE_LIB "./bin/engine_srv.so"
 #else
 	#define SERVER_LIB "server.dll"
-	#define ENGINE_LIB "engine.dll"
 #endif
 
 
 CGlobalVars *g_GlobalVars = NULL;
-IVEngineServer *g_EngineServer = NULL;
 IPlayerInfoManager *g_PlayerInfoManager = NULL;
 
 using namespace GarrysMod::Lua;
@@ -41,15 +35,9 @@ int RealFrameTime( lua_State* state )
 GMOD_MODULE_OPEN()
 {
 	CreateInterfaceFn serverFactory = Sys_GetFactory(SERVER_LIB);
-	CreateInterfaceFn engineFactory = Sys_GetFactory(ENGINE_LIB);
 
 	if (!serverFactory) {
 		LUA->ThrowError("error getting serverFactory!! (basically fuck you from Garry)");
-		return 0;
-	}
-
-	if (!engineFactory) {
-		LUA->ThrowError("error getting engineFactory!! (still basically fuck you from Garry)");
 		return 0;
 	}
 
@@ -59,20 +47,7 @@ GMOD_MODULE_OPEN()
 		return 0;
 	}
 
-	
-//	g_EngineServer = (IVEngineServer*)engineFactory(INTERFACEVERSION_VENGINESERVER, NULL);
-//	if (!g_EngineServer) {
-//		LUA->ThrowError("error getting g_EngineServer!! (still basically fuck you from Garry)");
-//		return 0;
-//	}
-	
-
-	ConColorMsg(Color(255, 255, 255, 255), "gmsv_fps by ");
-	ConColorMsg(Color(0, 50, 255, 255), "thelastpenguin\n");
-	ConColorMsg(Color(100, 100, 100, 255), "\tBuilt on " __DATE__ " at " __TIME__ "\n");
-
 	g_GlobalVars = g_PlayerInfoManager->GetGlobalVars();
-
 
 	LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB );
 
